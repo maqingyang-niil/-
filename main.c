4271b6271b90289e7c1147d1b96a566f5c4891b6
j'étudie pro gloriae virorum <131170107+maqingyang-niil@users.noreply.github.com>
2024-08-11 09:33:38 +0800

    Update main.c

--- main.c
+++ main.c
@@ -69,3 +69,66 @@ int main(void)
   }
   f_mount(NULL,"1:",1);
 }
+
+static FRESULT miscellaneous()
+{
+  DIR* dir;
+  FATFS* pfs;
+  FIL* fnew;
+  DWORD fre_clust,fre_sector,tot_sector;
+  FRESULT res_flash;
+  BYTE readBuffer[20];
+  UINT* fnum;
+  res_flash=f_getfree("1:",&fre_clust,&pfs);
+  /*总扇区数和空扇区数，用来计算使用率*/
+  tot_sector=(pfs->n_fatent-2)*pfs->csize;
+  fre_sector=pfs->free_clst*pfs->csize;
+  res_flash=f_open(fnew,"1:old",FA_CREATE_ALWAYS|FA_READ|FA_WRITE);
+  res_flash=f_write(fnew,"101100010",20,fnum);
+  if (res_flash==FR_OK)
+  {
+    res_flash=f_lseek(fnew,f_size(fnew)-1);
+    if (res_flash==FR_OK)
+    {
+      f_printf(fnew,"101100010");
+      res_flash=f_lseek(fnew,0);
+      res_flash=f_read(fnew,readBuffer,f_size(fnew),fnum);
+      if (res_flash==FR_OK)
+      {
+        //读取到的数据
+      }
+    }
+    f_close(fnew);
+    res_flash=f_opendir(dir,"1:testDir");
+    if (res_flash!=FR_OK)
+    {
+      res_flash=f_mkdir("1:testDir");
+
+    }
+    else
+    {
+      res_flash=f_closedir(&dir);
+      f_unlink("1:testDir/testdir.txt");
+    }
+    if (res_flash==FR_OK)
+    {
+      res_flash=f_rename("1:old","1:testDir/testdir.txt");
+      if (res_flash==FR_OK)
+      {
+
+      }
+      else
+      {
+
+      }
+    }
+  }
+  else
+  {
+
+  }
+  return res_flash;
+}
+
+
+
